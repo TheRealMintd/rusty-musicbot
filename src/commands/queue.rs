ba6a687799232ee0b38fd91f1fdf2b58d3a08e4f
Mintd <johnmax2468@gmail.com>
2021-11-10 14:42:17 +0800

    Split out queue function

--- src/commands/queue.rs
+++ src/commands/queue.rs
@@ -4,6 +4,7 @@ use serenity::{
 	model::channel::Message,
 	utils::{EmbedMessageBuilding, MessageBuilder},
 };
+use songbird::tracks::TrackHandle;
 
 use crate::utils::*;
 
@@ -19,40 +20,15 @@ async fn queue(ctx: &Context, msg: &Message) -> CommandResult {
 
 	match manager.get(guild.id) {
 		Some(handler_lock) => {
-			let mut queue_message = MessageBuilder::new();
 			let handler = handler_lock.lock().await;
-			handler.queue().current_queue().iter().enumerate().for_each(
-				|(index, metadata)| {
-					if index == 0 {
-						queue_message.push_mono("Now Playing");
-					} else {
-						queue_message.push_mono(index);
-					}
-					queue_message.push(" | ");
-
-					match metadata.metadata().source_url {
-						Some(ref url) => queue_message
-							.push_named_link_safe(metadata.get_title(), url),
-						None => {
-							queue_message.push_mono_safe(metadata.get_title())
-						}
-					};
-
-					queue_message.push("  ");
-					queue_message.push_mono_line(
-						metadata
-							.metadata()
-							.duration
-							.map(format_duration)
-							.as_deref()
-							.unwrap_or("No info"),
-					);
-				},
-			);
 
 			msg.channel_id
 				.send_message(&ctx.http, |m| {
-					m.embed(|e| e.description(queue_message))
+					m.embed(|e| {
+						e.description(build_queue_message(
+							&handler.queue().current_queue(),
+						))
+					})
 				})
 				.await?;
 		}
@@ -62,4 +38,35 @@ async fn queue(ctx: &Context, msg: &Message) -> CommandResult {
 	}
 
 	Ok(())
+}
+
+pub(crate) fn build_queue_message(queue: &[TrackHandle]) -> MessageBuilder {
+	let mut queue_message = MessageBuilder::new();
+	queue.iter().enumerate().for_each(|(index, metadata)| {
+		if index == 0 {
+			queue_message.push_mono("Now Playing");
+		} else {
+			queue_message.push_mono(index);
+		}
+		queue_message.push(" | ");
+
+		match metadata.metadata().source_url {
+			Some(ref url) => {
+				queue_message.push_named_link_safe(metadata.get_title(), url)
+			}
+			None => queue_message.push_mono_safe(metadata.get_title()),
+		};
+
+		queue_message.push("  ");
+		queue_message.push_mono_line(
+			metadata
+				.metadata()
+				.duration
+				.map(format_duration)
+				.as_deref()
+				.unwrap_or("No info"),
+		);
+	});
+
+	queue_message
 }
